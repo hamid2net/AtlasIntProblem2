316187a5f46661f440b5fa378e69886469f33b3d
H.Rahimi <H.Rahimi@RAHIMI>
2022-11-12 20:16:23 +0330

    Add project files.

---
+++ Problem2/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> openParen=new Stack<char>();
+            foreach(char c in s)
+            {
+                if(c == '(' || c== '{' || c=='[') openParen.Push(c);
+                else if(c == ')' && openParen.Count>0 && openParen.Peek()=='(') openParen.Pop();
+                else if (c == '}' && openParen.Count > 0 && openParen.Peek() == '{') openParen.Pop();
+                else if (c == ']' && openParen.Count > 0 && openParen.Peek() == '[') openParen.Pop();
+                else return false;
+            }
+            return true;
+        }
+    }
+}
